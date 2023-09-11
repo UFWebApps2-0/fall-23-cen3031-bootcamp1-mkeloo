7db4022875b1ee9981e16ee0758b9da767f6d84d
ufshoes4smiles <ufshoes4smiles@gmail.com>
2023-09-11 01:29:07 -0400

    finished the assignment

--- server.js
+++ server.js
@@ -1,60 +1,56 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    port = 8080;
+var http = require('http'),
+  fs = require('fs'),
+  port = 8080;
 
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
+var requestHandler = function (request, response) {
   /*Investigate the request object. 
     You will need to use several of its properties: url and method
   */
-  //console.log(request);
+  //   console.log(request.url, 'this is the url');
+  // console.log(
+  //   request.url === '/listings',
+  //   'this is http://127.0.0.1:8080/listings url.'
+  // );
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  //   console.log(request.method, 'this is the method');
+  // console.log(request.method === 'GET', 'this is the GET method');
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+  //   console.log(request.headers, 'this is the headers');
+  // console.log(request.headers, 'this is the headers');
 
-    */
+  //   Your request handler should send listingData in the JSON format as a response if a GET request is sent to the '/listings' path. Otherwise, it should send a 404 error.
+
+  // Check if it's a GET request to the path '/listings'
+  if (request.method === 'GET' && request.url === '/listings') {
+    // Set response header for JSON data and status 200 (OK)
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // send the listingData in the JSON format as a response
+    response.end(JSON.stringify(listingData));
+  } else {
+    // for everything else, send a 404 error
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('Not Found');
+  }
 };
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+fs.readFile('listings.json', 'utf8', function (err, data) {
+  // Check for errors
+  if (err) {
+    throw err;
+  }
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  // parse the read data and save it into the listingData variable
+  listingData = JSON.parse(data);
 
-    HINT: Read up on JSON parsing Node.js
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  // Create the server
+  server = http.createServer(requestHandler);
 
-    //Check for errors
-    /*this resource gives you an idea of the general format err objects and Throwing an existing object.
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
-   */
-  
-
-   //Save the data in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
-
+  // Start the server
+  server.listen(port, function () {
+    console.log('Server on: http://127.0.0.1:' + port);
+  });
 });
